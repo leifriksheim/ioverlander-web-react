3c4881fb15cb62020cf58691cbe4c0c1f3bd9a8a
Lucas Vogelsang <l@lucasvo.com>
2017-08-03 16:15:19 -0400

    Fix asset url in staging

--- config/staging.js
+++ config/staging.js
@@ -3,7 +3,7 @@ module.exports = {
   assets: {
       compileAssets: true,
       host: 'ec2-54-173-20-206.compute-1.amazonaws.com:3001',
-      urlPrefix: 'http://ec2-54-173-20-206.compute-1.amazonaws.com:3001'
+      urlPrefix: 'http://ec2-54-173-20-206.compute-1.amazonaws.com:3001/'
   },
 
   sessionSettings: {
