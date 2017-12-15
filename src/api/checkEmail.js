d42b0c85d603a2c5ab0eb6d75dcb1fd3f7eabc5e
Lucas Vogelsang <l@lucasvo.com>
2017-12-14 23:18:49 -0500

    Delete server stuff

--- src/api/checkEmail.js
+++
@@ -1,9 +0,0 @@
-export default (models) => function (req, res) {
-  models.users.count({
-    where: {
-      email: req.body.email
-    }
-  }).then((count) => {
-    res.json({ exists: !!count })
-  })
-}
